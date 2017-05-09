8525f78e5a02fad698a1eb77ce3e1a8422751d82
Michael Eboagu <Michael Eboagu>
2017-05-09 12:28:48 +0100

    findMinMax function without js sort() method

--- app/findMinMax.js
+++ app/findMinMax.js
@@ -1,18 +1,40 @@
 module.exports = {
-	findMinMax: (list) =>{
-		const sortedlist = list.sort(function(a, b){return a-b});
-		let newList = [];
-		let min = sortedlist[0];
-		let max = sortedlist[(list.length-1)];
-		if(min === max){
-			//newList.push(min || max);
-			return [min];
-		}
-		newList.push(min);
-		newList.push(max);
 
+	findMinMax: (list) => {
+			let newList = [];
+			
+	        if(typeof(list) != 'object'){
+	            return 'invalid input';
+	        }
+			list = list.sort();
+	    	list.sort();
+	    	let min = list[0] ;
+	    	let max =  list[0];
+	    	for(let count = 0; count < list.length ; count++){
+	      		
+	            if (list[count] > max ){
+	             	
+	                max = list[count];
+	             	
+	                min = list[0];
+	         	    
+	                newList[0] = min ;
+	         	    
+	                newList[1] = max ;
+	         	
+	     		}
+	     		else if(list[i] == max ){
+	     			
+	                newList[0] = min ;
 
-		return newList;
+	     		}
+
+	    	}
+			
+			
+	    
+			return newList;
+			
+
 	}
 }
-
